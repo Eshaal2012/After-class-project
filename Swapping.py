f80c44a6642e0d6b8e5518581dc0b81dea567084
Eshaal2012 <eshaalfarrukh48@gmail.com>
2025-01-19 18:17:33 +0500

    Swapping

---
+++ Swapping.py
@@ -0,0 +1,14 @@
+x=input("Enter value of x :")
+y=input("Enter value of y :")
+z=input("Enter value of z :")
+c=input("Enter value of c :")
+swap=x
+x=y
+y=swap
+z=c
+c=z
+print("Value of x after swapping",x)
+print("Value of y after swapping",y)
+print("Value of z after swapping",z)
+print("Value of c after swapping",c)
+      
